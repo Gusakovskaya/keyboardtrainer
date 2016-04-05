a1bbcb85ab77244dfe39c89d0263af8a284779cf
Zhenya <zhenya.gusakovskaya@mail.ru>
2016-04-05 23:54:01 +0300

    Окно и ввод.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,47 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setCentralWidget(ui->stackedWidget);
+    ui->mess2->hide();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_exit_clicked()
+{
+    close();
+}
+
+void MainWindow::on_start_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_input_textChanged(const QString &str)
+{
+    QString task = ui->task->text();
+    int n = 0;
+    for (int i = 0, j = 0; i < str.size(); i++, j++){
+        if (task[i] != str[j]){
+            ui->input->backspace();
+            n++;
+        }
+    QString result = "";
+    if (n == 0)
+            result = "Молодец! Ты не допустил ни одной ошибки. Так держать!";
+    else
+        result.append("У тебя");
+        result.append(n);
+        result.append("ошибок.");
+    ui->mess2->setText(result);
+    ui->mess2->show();
+    }
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,15 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_exit_clicked();
+
+    void on_start_clicked();
+
+    void on_input_textChanged(const QString &arg1);
+
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
